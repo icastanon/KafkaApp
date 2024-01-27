86fc330b9f27ba72ce9e56a20ec71bdbe1e0c87a
Ivan Castanon <ivan.alberto.castanon@gmail.com>
2024-01-27 14:42:17 -0600

    Adding endpoints for templates

--- kafkaapp/src/main/java/com/ivan/kafkaapp/repository/KafkaMessageTemplateRepository.java
+++ kafkaapp/src/main/java/com/ivan/kafkaapp/repository/KafkaMessageTemplateRepository.java
@@ -1,5 +1,7 @@
 package com.ivan.kafkaapp.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,10 @@ import com.ivan.kafkaapp.entity.KafkaMessageTemplate;
 @Repository
 public interface KafkaMessageTemplateRepository extends JpaRepository<KafkaMessageTemplate, Long> {
 	
-	public KafkaMessageTemplate findByTemplateId(Integer templateId);
+	public KafkaMessageTemplate findByTemplateId(int templateId);
+	
+	public List<KafkaMessageTemplate> findAll();
+	
+	public int deleteByTemplateId(int templateId);
 
 }
